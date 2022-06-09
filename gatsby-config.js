464ec6fe5a269bb4f8921ea881df23e9e7ee3bb3
youmeokay <thealexhitchcock@gmail.com>
2022-06-09 18:36:48 +0200

    frontend changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        // apiURL: "https://allianzfoundationbackend.herokuapp.com",
-        // token: "5de6c6aad33be64a8075093d16df49bce45db2013febca7914c15f3e7385263f9a695657e38842a79eb456993fc6f9bf79e2f7b466d3428560b4085cac88985be3e2edef108a54962a3831d1aebf9eb6e08b9cc8adb28d4309febc6094063127f8dbab8eb90e4de1a731332c4a8b02568820890f88657bd07945a6bbd9cae873",
-        apiURL: "http://localhost:1337",
-        token: "a0185876ba21ee588db0e0dc4d8176f26fc71748ffff242a9fa3e8dc2dc118d4203763bf585fe84b59e7a857682dfc28b09d5d6900cf1da41b21eca1ebc34fdccf50fb959b70b0859ef7179cb2685a1aecda80cbc861b142fb09cdfdb619e64fbb2a96ffd2615da63ff473980333398b431e8d745eea5631cc14d92e7c00dc17",
+        apiURL: "https://allianzfoundationbackend.herokuapp.com",
+        token: "5de6c6aad33be64a8075093d16df49bce45db2013febca7914c15f3e7385263f9a695657e38842a79eb456993fc6f9bf79e2f7b466d3428560b4085cac88985be3e2edef108a54962a3831d1aebf9eb6e08b9cc8adb28d4309febc6094063127f8dbab8eb90e4de1a731332c4a8b02568820890f88657bd07945a6bbd9cae873",
+        // apiURL: "http://localhost:1337",
+        // token: "a0185876ba21ee588db0e0dc4d8176f26fc71748ffff242a9fa3e8dc2dc118d4203763bf585fe84b59e7a857682dfc28b09d5d6900cf1da41b21eca1ebc34fdccf50fb959b70b0859ef7179cb2685a1aecda80cbc861b142fb09cdfdb619e64fbb2a96ffd2615da63ff473980333398b431e8d745eea5631cc14d92e7c00dc17",
         collectionTypes: ['article', 'tag'],
         singleTypes: ['about', 'global', 'index'],
         preview: true,
