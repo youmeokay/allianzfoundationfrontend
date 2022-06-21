34633ec4ed413430cebbdb2636625c67925f57e3
youmeokay <thealexhitchcock@gmail.com>
2022-06-21 13:00:31 +0200

    modules: highlight fix

--- src/components/modules/Highlight.js
+++ src/components/modules/Highlight.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-const ModuleHighlight = ({ data }) => {
+const Highlight = ({ data }) => {
   return (
     <article className="module-highlight cell medium-6 revealer" key={`highlight__${data.strapi_id}`}>
-        {data.file &&  <a href={data.file.url} target={data.highlightLinktarget} className="highlight-link"><div className="highlight">{data.highlightTagline &&  <p>{data.highlightTagline}</p>}{data.highlightTitle && <h2 className="h1 highlight-title">{data.highlightTitle}</h2>}</div></a>}
-        {data.highlightLink &&  <a href={data.highlightLink} target={data.highlightLinktarget} className="highlight-link"><div className="highlight">{data.highlightTagline &&  <p>{data.highlightTagline}</p>}{data.highlightTitle && <h2 className="h1 highlight-title">{data.highlightTitle}</h2>}</div></a>}
+        {data.file &&  <a href={data.file.file.url} target={`_${data.highlightLinktarget}`} className="highlight-link"><div className="highlight">{data.highlightTagline &&  <p>{data.highlightTagline}</p>}{data.highlightTitle && <h2 className="h1 highlight-title">{data.highlightTitle}</h2>}</div></a>}
+        {data.highlightLink &&  <a href={data.highlightLink} target={`_${data.highlightLinktarget}`} className="highlight-link"><div className="highlight">{data.highlightTagline &&  <p>{data.highlightTagline}</p>}{data.highlightTitle && <h2 className="h1 highlight-title">{data.highlightTitle}</h2>}</div></a>}
     </article>
   )
 }
 
-export default ModuleHighlight
+export default Highlight
